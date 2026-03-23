d285807854b7ef0888b137e75bab1d92482b09d3
Saneha001 <sanehabhadyar@gmail.com>
2026-03-23 11:50:11 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,91 @@
+let gameSeq=[];
+let userSeq=[];
+
+let btns=['yellow','red','purple','green'];
+let started=false;
+let level=0;
+let highscore=0;
+
+let h2=document.querySelector('h2');
+document.addEventListener('keypress',function(){
+    if(started==false){
+        console.log("game is started");
+        started=true;
+        levelUp();
+    }
+});
+ function gameflash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    } ,250);
+}
+
+function userflash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    } ,250);
+}
+
+
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`level ${level}`;
+    if(level>highscore){
+        highscore=level;
+    }
+
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=btns[randIdx];
+    let randbtn=document.querySelector(`.${randColor}`);
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randbtn);
+gameSeq.push(randColor);
+console.log(gameSeq);
+
+
+    gameflash(randbtn);
+}
+ function checkans(idx){
+    if(userSeq[idx]==gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+
+    }
+    else{
+        h2.innerHTML=`Game over! Your score was <b>${level}</b> <br>Highscore:<b>${highscore}</b><br> Press any key to start the game.`;
+        document.querySelector('body').style.backgroundColor='red';
+        setTimeout(function(){
+ document.querySelector('body').style.backgroundColor='white';
+        },150);
+
+        reset();
+    }
+ }
+
+
+function btnpress(){
+    let btn=this;
+    userflash(btn);
+    userColor=btn.getAttribute('id');
+   userSeq.push(userColor);
+
+
+   checkans(userSeq.length-1);
+}
+let allbtns=document.querySelectorAll(".btn");
+for(btn of allbtns){
+    btn.addEventListener("click",btnpress);
+}
+
+
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
